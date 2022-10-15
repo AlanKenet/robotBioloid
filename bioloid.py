ca83803db8b3a53ccdc5265b1faceed3e3f89864
AlanKenet <alankenet@gmail.com>
2022-10-15 01:03:32 -0600

    *Reformulacion de calculos TH: t = t_theta@t_d@t_a@t_alpha segun la propiedad asociativa de las matrices involucradas. *Reacomodo de todos los parametros DH para coincidir con el nuevo orden de calculo de TH. *Calculos de marcos de referencia terminado. *Personalizacion de lineas y marcadores del ploteo del robot *Limpieza de codigo basura

--- bioloid.py
+++ bioloid.py
@@ -5,10 +5,10 @@ from matplotlib import pyplot as plt
 # import matplotlib.animation as animation
 
 def calcTH(paramsDH):
-    a = paramsDH[0]
-    alpha = paramsDH[1]
-    d = paramsDH[2]
-    theta = paramsDH[3]
+    theta = paramsDH[0]
+    d = paramsDH[1]
+    a = paramsDH[2]
+    alpha = paramsDH[3]
 
     r_I = np.identity(3)
 
@@ -22,14 +22,14 @@ def calcTH(paramsDH):
 
     p_0 = np.array( np.zeros( (3,1) ) )
 
-    p_d = np.array( ([0],[0],[d]) )
+    p_d = np.array( ([0], [0], [d]) )
 
-    p_a = np.array( ([a],[0],[0]) )
+    p_a = np.array( ([a], [0], [0]) )
 
+    t_theta = np.vstack(  [np.hstack( [r_theta,p_0] ), np.array( [0, 0, 0, 1] ) ] )
+    t_d     = np.vstack(  [np.hstack( [r_I,p_d] )    , np.array( [0, 0, 0, 1] ) ] )
     t_a     = np.vstack(  [np.hstack( [r_I,p_a] )    , np.array( [0, 0, 0, 1] ) ] )
     t_alpha = np.vstack(  [np.hstack( [r_alpha,p_0] ), np.array( [0, 0, 0, 1] ) ] )
-    t_d     = np.vstack(  [np.hstack( [r_I,p_d] )    , np.array( [0, 0, 0, 1] ) ] )
-    t_theta = np.vstack(  [np.hstack( [r_theta,p_0] ), np.array( [0, 0, 0, 1] ) ] )
 
     t = t_theta @ t_d @ t_a @ t_alpha
 
@@ -39,19 +39,20 @@ GDL = 6
 
 L = [20,20,20,20]
 
-# Q = [pi/4, pi/4, pi/4, pi/4, pi/4, pi/4]
-Q = [0, 0, 0, 0, 0, 0]
+Q = [pi/4, pi/4, pi/4, pi/4, pi/4, pi/4]
+# Q = [0, 0, 0, 0, 0, 0]
 # Q = [pi/2, pi/2, pi/2, pi/2, pi/2, pi/2]
 
 posFin = np.zeros( (3,GDL+1) )
 
-dH  = np.array([ [    20,     0,    0,       Q[0] ],
-                 [    20,     0,    0,       Q[1] ],
-                 [    20,     0,    0,       Q[2] ],
-                 [    20,     0,    0,       Q[3] ],
-                 [    20,     0,    0,       Q[4] ],
-                 [    20,     0,    0,       Q[5] ], ])
-    
+#DH parametros = [theta, d,  a, alpha]
+dH  = np.array([ [ Q[0], 0, 20,    0 ],
+                 [ Q[1], 0, 20,    0 ],
+                 [ Q[2], 0, 20,    0 ],
+                 [ Q[3], 0, 20,    0 ],
+                 [ Q[4], 0, 20,    0 ],
+                 [ Q[5], 0, 20,    0 ], ])
+
 A = np.zeros( (GDL, 4, 4) )
 
 H = np.zeros( (GDL+1, 4, 4) )
@@ -76,65 +77,36 @@ for i in range(GDL):
     for j in range(3):
         posFin[j,i+1] = H[i+1,j,3]
 
-plt.plot(posFin[0],posFin[1],posFin[2],'o-',lw=2)
+plt.plot(posFin[0], posFin[1], posFin[2], lw = 2, color = 'indigo',
+         marker = 'o', markerfacecolor = 'mediumpurple', markeredgecolor = 'mediumpurple')
 
 
 
-posRef = np.zeros( (3,3,2) )
+posRef = np.zeros( (3, 3, 2) )
 
-tamRef = 10
+tamRef = 12
 
-colores = ['y', 'g', 'r']
+colores = ['red', 'gold', 'chartreuse']
 
-ARef = np.zeros( (4, 4) )
+ARef = np.zeros( (3, 4, 4) )
+ARef[0] = np.array( calcTH( [     0,      0, tamRef, 0 ] ) )
+ARef[1] = np.array( calcTH( [(pi/2),      0, tamRef, 0 ] ) )
+ARef[2] = np.array( calcTH( [     0, tamRef,      0, 0 ] ) )
 
-ARef = np.array( calcTH( [tamRef, 0, 0, 0 ] ) ).reshape(4,4)
-
-HRef = np.zeros( (3,4, 4) )
+HRef = np.zeros( (3, 4, 4) )
 
 for i in range(GDL):
-    HRef[0] = np.array( calcTH( [ dH[i,0],        dH[i,1], dH[i,2],        dH[i,3] ] ) )
-    HRef[1] = np.array( calcTH( [ dH[i,0],        dH[i,1], dH[i,2], (pi/2)+dH[i,3] ] ) )
-    HRef[2] = np.array( calcTH( [ dH[i,0], (pi/2)+dH[i,1], dH[i,2], (pi/2)+dH[i,3] ] ) )
+    for j in range(3):
+        HRef[j] = H[i+1] @ ARef[j]
 
+        for k in range(3):
+            posRef[j,k,0] = H[i+1,k,3]
+            posRef[j,k,1] = HRef[j,k,3]
 
-    for j in range(2):
-        print(HRef[j])
-        print('')
+        plt.plot([posRef[j,0,0], posRef[j,0,1]],
+                 [posRef[j,1,0], posRef[j,1,1]],
+                 [posRef[j,2,0], posRef[j,2,1]], colores[j])
 
-        HRef[j] = H[i] @ HRef[j]
-
-        HRef[j] = HRef[j] @ ARef
-
-        # for k in range(3):
-        #     posFin[j,i+1] = H[i+1,j,3]
-
-    # plt.plot(posFin[0],posFin[1],posFin[2],'o-',lw=2)
-
-
-    # for j in range(3):
-    #     HRef = np.dot( H[k+1], ARef[j] )
-
-    #     print(ARef[j])
-    #     print(H[k+1])
-    #     print(HRef)
-    #     print('')
-
-    #     for i in range(2):
-    #         posRef[j,i] = [ H[k+1,j,3], HRef[i,3] ]
-
-    #         print(H[k+1,j,3], HRef[i,3])
-    #         print(posRef[j,i])
-    #         print('')
-
-
-    #     # print('')
-    #     print(posRef[j])
-    #     print('')
-    #     print('')
-    #     plt.plot([posRef[j,0,0], posRef[j,0,1]],
-    #              [posRef[j,1,0], posRef[j,1,1]],
-    #              [posRef[j,2,0], posRef[j,2,1]], colores[j])
 
 
 plt.show()
